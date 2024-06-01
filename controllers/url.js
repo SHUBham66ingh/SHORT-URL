098596addbed4bdf1b39bda180065b7bb89ff2ec
Shubham singh <shubham1041singh@gmail.com>
2024-06-01 21:20:16 +0530

    ......

--- controllers/url.js
+++ controllers/url.js
@@ -4,7 +4,7 @@ const URL = require('../models/url');
 
 async function handleGenerateNewShortURL(req,res){
 const body = req.body;
-if(!body.url) return res.status(400).json({error:'url'})
+if(!body.url) return res.status(400).json({error:'url is required'})
 
 const shortID = shortid(8);
 await URL.create({
